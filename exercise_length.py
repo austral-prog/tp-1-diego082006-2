1b1f5c17f641affa6dd60b62983b03328528eb37
diego082006 <ddelgado1@mail.austral.edu.ar>
2026-03-12 14:04:23 -0300

    Update exercise_length.py

--- exercise_length.py
+++ exercise_length.py
@@ -9,3 +9,15 @@ def length():
     4. Pulgadas (1 pulgada ≈ 0.0254 m)
     """
     metros = 1000
+    un_kilometro = 1000
+    una_milla = 1609.34
+    un_pie = 0.3048
+    una_pulgada = 0.0254
+    distancia_kilometros = metros / un_kilometro
+    distancia_millas = metros / una_milla
+    distancia_pies = metros / un_pie
+    distancia_pulgadas = metros / una_pulgada
+    print(distancia_kilometros)
+    print(distancia_millas)
+    print(distancia_pies)
+    print(distancia_pulgadas)
