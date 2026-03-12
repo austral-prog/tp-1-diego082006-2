779d0d9dd2bc87cf35f45e81431fba94808576b7
diego082006 <ddelgado1@mail.austral.edu.ar>
2026-03-12 11:40:56 -0300

    Add basic arithmetic operations and print results

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,17 @@ def math():
     """
     a = 57
     b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a*b
+    cociente_entero = a//b
+    promedio = a/b
+    resto_de_la_division_entera = a%b
+    valor_real_de_la_division = (a + b)/2
+    print(suma)
+    print(diferencia)
+    print(producto)
+    print(valor_real_de_la_division)
+    print(cociente_entero)
+    print(resto_de_la_division_entera)
+    print(promedio)
